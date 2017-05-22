0988a1ea7647321868c0cde723336deaf279e21b
Maurizio Zucchelli <mauzuc90@yahoo.it>
2017-05-22 18:51:42 +0100

    Add solution for Append and Delete
    https://www.hackerrank.com/challenges/append-and-delete

---
+++ src/append_and_delete/SolutionTest.java
@@ -0,0 +1,19 @@
+package append_and_delete;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SolutionTest {
+    @Test
+    void example1() {
+        boolean canTransform = Solution.canTransform("hackerhappy", "hackerrank", 9);
+        assertTrue(canTransform);
+    }
+
+    @Test
+    void example2() {
+        boolean canTransform = Solution.canTransform("aba", "aba", 7);
+        assertTrue(canTransform);
+    }
+}

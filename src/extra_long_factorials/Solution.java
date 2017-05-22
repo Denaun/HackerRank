312e109386dc4318d80a7da24993c172890f2fbf
Maurizio Zucchelli <mauzuc90@yahoo.it>
2017-05-22 18:50:43 +0100

    Add solution for Extra Long Factorials
    https://www.hackerrank.com/challenges/extra-long-factorials

---
+++ src/extra_long_factorials/Solution.java
@@ -0,0 +1,20 @@
+package extra_long_factorials;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Solution {
+    static BigInteger factorial(int n) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 2; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        System.out.print(factorial(num));
+    }
+}

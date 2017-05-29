9373f094c371b2df42e6d1ea61f738f05707cdec
Maurizio Zucchelli <mauzuc90@yahoo.it>
2017-05-29 08:43:51 +0100

    Add dumb solution.
    Goes through every cell, might skip borders if unneeded.

--- src/main/java/artificialintelligence/botbuilding/botcleanpartiallyobservable/Solver.java
+++ src/main/java/artificialintelligence/botbuilding/botcleanpartiallyobservable/Solver.java
@@ -1,23 +1,54 @@
 package artificialintelligence.botbuilding.botcleanpartiallyobservable;
 
-import artificialintelligence.botbuilding.Action;
-import artificialintelligence.botbuilding.Coordinates;
+import artificialintelligence.botbuilding.*;
 
 class Solver {
     private Coordinates start;
     private Map map;
+    private Action nextAction;
 
     Solver(Coordinates start, Map map) {
         this.start = start;
         this.map = map;
+        this.nextAction = null;
     }
 
     boolean solve() {
+        if (map.isDirty(start.getX(), start.getY())) {
+            nextAction = new Clean();
+            return true;
+        }
+        Direction verticalDirection;
+        if (start.getX() % 2 == 0) {
+            verticalDirection = Direction.DOWN;
+        } else {
+            verticalDirection = Direction.UP;
+        }
+        Coordinates nextPos = new Coordinates(start).move(verticalDirection);
+        if (nextPos.getY() > 0 && nextPos.getY() < map.size() - 1) {
+            nextAction = new Move(verticalDirection);
+            return true;
+        }
+        if (nextPos.getY() == 0 || nextPos.getY() == map.size() - 1) {
+            if (map.isDirty(nextPos.getX(), nextPos.getY())) {
+                nextAction = new Move(verticalDirection);
+                return true;
+            }
+            if (nextPos.getX() < map.size() - 1 &&
+                map.isDirty(nextPos.getX() + 1, nextPos.getY())) {
+                nextAction = new Move(verticalDirection);
+                return true;
+            }
+        }
+        if (nextPos.getX() < map.size()) {
+            nextAction = new Move(Direction.RIGHT);
+            return true;
+        }
+        nextAction = null;
         return false;
     }
 
     Action getNextMove() {
-        return new Action() {
-        };
+        return nextAction;
     }
 }
--- src/test/java/artificialintelligence/botbuilding/botcleanpartiallyobservable/SolverTest.java
+++ src/test/java/artificialintelligence/botbuilding/botcleanpartiallyobservable/SolverTest.java
@@ -1,9 +1,6 @@
 package artificialintelligence.botbuilding.botcleanpartiallyobservable;
 
-import artificialintelligence.botbuilding.Action;
-import artificialintelligence.botbuilding.Coordinates;
-import artificialintelligence.botbuilding.Direction;
-import artificialintelligence.botbuilding.Move;
+import artificialintelligence.botbuilding.*;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -26,5 +23,50 @@ public class SolverTest {
         Move move = (Move) action;
         Assert.assertTrue(
                 move.getDirection() == Direction.RIGHT || move.getDirection() == Direction.DOWN);
+    }
+
+    @Test
+    public void testSolve1() {
+        Map map = new Map(5);
+        map.notifyClean(0, 0);
+        map.notifyDirty(1, 0);
+        map.notifyClean(2, 0);
+        map.notifyClean(0, 1);
+        map.notifyDirty(1, 1);
+        map.notifyClean(2, 1);
+        map.notifyClean(0, 2);
+        map.notifyClean(1, 2);
+        map.notifyClean(2, 2);
+        Solver solver = new Solver(new Coordinates(1, 1), map);
+        boolean found = solver.solve();
+        Assert.assertTrue(found);
+        Action action = solver.getNextMove();
+        Assert.assertTrue(action instanceof Clean);
+
+        map.notifyClean(1, 1);
+        found = solver.solve();
+        Assert.assertTrue(found);
+        action = solver.getNextMove();
+        Assert.assertTrue(action instanceof Move);
+        Move move = (Move) action;
+        Assert.assertTrue(move.getDirection() == Direction.UP);
+    }
+
+    @Test
+    public void testSolve2() {
+        Map map = new Map(5);
+        map.notifyClean(0, 0);
+        map.notifyClean(1, 0);
+        map.notifyClean(2, 0);
+        map.notifyClean(0, 1);
+        map.notifyClean(1, 1);
+        map.notifyClean(2, 1);
+        Solver solver = new Solver(new Coordinates(1, 0), map);
+        boolean found = solver.solve();
+        Assert.assertTrue(found);
+        Action action = solver.getNextMove();
+        Assert.assertTrue(action instanceof Move);
+        Move move = (Move) action;
+        Assert.assertTrue(move.getDirection() == Direction.RIGHT);
     }
 }
